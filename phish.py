b19029673220a97dc3c8840f51e96dbccfa5b5ef
avinashsinghgit <as9183181@gmail.com>
2023-08-08 13:06:24 +0530

    004

---
+++ phish.py
@@ -0,0 +1,63 @@
+import re
+import requests
+import pandas as pd
+
+
+import whois
+import datetime
+
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
+
+# HTML and Javascript Based Features
+import warnings
+
+# Domain based features
+import whois
+from datetime import datetime
+
+
+# EDA
+import matplotlib.pyplot as plt
+
+
+# Normalization
+from sklearn.preprocessing import MinMaxScaler
+
+# Training
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import pickle
+
+
+
+###############********************* EDA *******************############
+
+df = pd.read_csv("/Users/avinash/Desktop/phising_dataset.csv").iloc[:,1:]
+df1 = df.iloc[:,:-1]
+x = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+scaler=MinMaxScaler()  
+scaler.fit(x.values)  
+X_scaled=scaler.transform(x.values)   
+X_new = pd.DataFrame(X_scaled,columns=x.columns)
+X_test,X_train,y_test,y_train = train_test_split(X_new,y,test_size=0.2,random_state=50,shuffle=True,stratify=y)
+
+
+
+###############********************* Decision Tree *******************############
+tree = DecisionTreeClassifier(max_depth = 5)
+tree.fit(X_train, y_train)
+y_test_tree = tree.predict(X_test)
+y_train_tree = tree.predict(X_train)
+acc_train_tree = accuracy_score(y_train,y_train_tree)
+acc_test_tree = accuracy_score(y_test,y_test_tree)
+
+
+pickle.dump(tree, open('model.pkl','wb'))
+
+
+
--- phish/views.py
+++ phish/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
 # Create your views here.
 
+from joblib import dump
+import os
+
+import pickle
+
+
 
 def index(request):
     return render(request,"index.html")
@@ -13,11 +19,11 @@ def about(request):
 
 def ip(url):
     try:
-        ip_check = url.split("//")[1].split("/")[0]  # extracts the domain
-        if (ip_check.replace('.','').replace(':','').isnumeric() == True):    # checks whether the domain name contains only numerical values
-            return -1
-        if (ip_check.replace('.', '').replace(':','').isalnum() == True):         # to check in case of hexadecimal IP address value
-            return -1
+        domain = url.split("//")[1].split("/")[0]  # Extracts the domain
+        if domain.replace('.', '').replace(':', '').isnumeric():
+            return -1  # If the domain name contains only numerical values
+        if domain.isalnum():
+            return -1  # To check in case of hexadecimal IP address value
         else:
             return 1
     except:
@@ -25,19 +31,25 @@ def ip(url):
 
 
 def search(request):
+
+
     if request.method == "POST":
+
+        model=pickle.load(open('model.pkl','rb'))
+        
         query = request.POST['q']  # Get the 'query' parameter from the URL
         print(query)
         print("phish to be upcoming")
-        
+            
         results = ip(query)  # Call the Python function to process the query
         print(results)
+
 
         if results == -1:
             dict_output = {"Result" : "Phising"}
         elif results == 0:
             dict_output = {"Result" : "Suspicious"}
-        else:
+        if results == 1:
             dict_output = {"Result" : "Legitmate"}
 
         return render(request, "result.html", {'results':dict_output})
